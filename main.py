85169443475a244df9b689bb957f0162105f5558
raghav2005 <raghavawasthi2005@gmail.com>
2021-06-30 17:28:02 +0400

    added basic platform code

--- main.py
+++ main.py
@@ -103,6 +103,7 @@ class Player(MasterSprite):
 	def __init__(self, x, y):
 
 		super().__init__('images/ninja.png')
+
 		self.rotate = -15
 		self.image = pygame.transform.rotate(self.image, self.rotate)
 		self.og_image = self.image
@@ -196,6 +197,24 @@ class Player(MasterSprite):
 
 		if self.rect.y > (display_height - 68):
 			self.rect.y = display_height - 68
+
+class Platform(MasterSprite):
+	def __init__(self, x, y, w, h):
+		
+		super().__init__('images/platform_blue.png')
+
+		self.w = w
+		self.h = h
+
+		self.image = pygame.transform.scale(self.image, (self.w, self.h))
+		self.og_image = self.image
+
+		self.set_xy(x, y)
+
+	def change_pic(self, new_img):
+
+		self.image = pygame.image.load(new_img)
+		self.image = pygame.transform.scale(self.image, (self.w, self.h))
 
 # quickly create objects (rects)
 def text_objects(text, font, colour):
@@ -493,7 +512,8 @@ class Main(Screen):
 		self.clear_screen()
 
 		user = Player(368, 732)
-
+		pt1 = Platform(200, 200, 100, 20)
+	
 		display.blit(main_bg, (0, 0))
 
 		while True:
@@ -519,6 +539,7 @@ class Main(Screen):
 
 			display.blit(main_bg, (0, 0))
 			display.blit(user.get_surface(), user.get_rect())
+			display.blit(pt1.get_surface(), pt1.get_rect())
 			
 			self.manager.draw_ui(display)
 			pygame.display.flip()
